873959d2498badb23a8e0938069edb54e19abba9
back7 <back7@dev.lan>
2022-07-14 23:02:19 +0300

    Added models Category and Transaction.

--- core/categories/models.py
+++ core/categories/models.py
@@ -1,6 +1,6 @@
 import enum
 
-from sqlalchemy import Column, Integer, String, Enum, ForeignKey
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
@@ -17,10 +17,14 @@ class Category(Base):
     __tablename__ = "category"
 
     id: int = Column("id", Integer, primary_key=True)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
-    name: str = Column("name", String, unique=True, nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False, index=True)
+    name: str = Column("name", String, nullable=False)
     type: enum.Enum = Column("type", Enum(CategoryType), nullable=False)
 
     user = relationship("User", back_populates="categories")
     transactions_from = relationship("Transaction", back_populates="category_from")
     transactions_to = relationship("Transaction", back_populates="category_to")
+
+    __table_args__ = (
+        UniqueConstraint('user_id', 'name', name='user_category_uc'),
+    )
--- core/transactions/models.py
+++ core/transactions/models.py
@@ -1,25 +1,32 @@
 import datetime
+import enum
 import uuid
 from decimal import Decimal
 
-from sqlalchemy import Column, Numeric, DateTime, ForeignKey, Integer
+from sqlalchemy import Column, Numeric, DateTime, ForeignKey, Integer, Enum
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
 from settings.db import Base
 
 
+class TransactionType(enum.Enum):
+    INCOME = "income"
+    EXPENSE = "expense"
+    TRANSFER = "transfer"
+
+
 class Transaction(Base):
     __tablename__ = "transaction"
 
     id: uuid.UUID = Column("id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
-    category_from_id = Column(Integer, ForeignKey("category.id"), nullable=False)
-    category_to_id = Column(Integer, ForeignKey("category.id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False, index=True)
+    category_from_id = Column(Integer, ForeignKey("category.id"), nullable=False, index=True)
+    category_to_id = Column(Integer, ForeignKey("category.id"), nullable=False, index=True)
     amount: Decimal = Column("amount", Numeric(16, 2), nullable=False)
-    created: datetime.datetime = Column("created", DateTime, default=datetime.datetime.now, nullable=False) # TODO add index and ordering
+    type: enum.Enum = Column("type", Enum(TransactionType), nullable=False, index=True)
+    created: datetime.datetime = Column("created", DateTime, default=datetime.datetime.now, nullable=False, index=True)
 
     user = relationship("User", back_populates="transactions")
     category_from = relationship("Category", back_populates="transactions_from")
     category_to = relationship("Category", back_populates="transactions_to")
-
